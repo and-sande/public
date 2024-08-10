ed0a6539a2ec687ad1d7e6e8e34f3453bd75bcd6
and-sande <114400088+and-sande@users.noreply.github.com>
2024-08-10 10:43:28 +0200

    12

--- audi.py
+++ audi.py
@@ -1,3 +1,13 @@
+try:
+    import undetected_chromedriver as uc
+except ImportError:
+    import subprocess
+    subprocess.check_call(["python", "-m", "pip", "install", "undetected-chromedriver"])
+    import undetected_chromedriver as uc
+
+# Your existing code here
+
+
 import re
 import pandas as pd
 from selenium import webdriver
